cc8605e0042b18b3f220a3378a35d8bb37a5d988
hanseulchoi <lotte.hanseul@gmail.com>
2022-06-22 09:31:14 +0900

    4. Hilt in yout Application class, HiltAndroidApp 어노테이션 추가

--- android-hilt/android-hilt-main/app/src/main/java/com/example/android/hilt/LogApplication.kt
+++ android-hilt/android-hilt-main/app/src/main/java/com/example/android/hilt/LogApplication.kt
@@ -17,7 +17,11 @@
 package com.example.android.hilt
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+// 4: HiltAndroidApp 어노테이션은 Hilt의 베이스 코드들을 생성시켜준다. 또한, Parent Container의 역할로
+// 최상단에서 의존성을 제공해주는 역할을 한다. => 즉, 모든 Container들이 이 클래스에 접근이 가능하다.
+@HiltAndroidApp
 class LogApplication : Application() {
 
     lateinit var serviceLocator: ServiceLocator
