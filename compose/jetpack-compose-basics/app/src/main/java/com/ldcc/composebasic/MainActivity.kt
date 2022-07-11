1d6e86e08bdceaf916f0729c6469ff100714c60a
hanseulchoi <lotte.hanseul@gmail.com>
2022-07-11 13:28:56 +0900

    3. Getting started with Compose, 초기 화면 설명 주석 작업 진행

--- compose/jetpack-compose-basics/app/src/main/java/com/ldcc/composebasic/MainActivity.kt
+++ compose/jetpack-compose-basics/app/src/main/java/com/ldcc/composebasic/MainActivity.kt
@@ -12,12 +12,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ldcc.composebasic.ui.theme.ComposeBasicTheme
 
+// 3. Activity는 AndroidApp의 EntryPoint이다. setContent를 통해 layout을 정의한다.
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // 3. ComposeBasicTheme는 안드로이드의 스타일 테마를 정의한다.
             ComposeBasicTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -29,11 +30,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// 3. @Composable : Compose Function임을 알려주고 있는 어노테이션
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+// 3. Preview 어노테이션의 경우, 미리보기 화면을 제공한다.
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
