b25d5b6f4475344f1c0d1c83067e5811ca0dcf9c
hanseulchoi <lotte.hanseul@gmail.com>
2022-07-11 14:45:24 +0900

    4. Tweaking the UI, Surface로 색상 변경 및 Modifier를 통한 UI 배치 작업

--- compose/jetpack-compose-basics/app/src/main/java/com/ldcc/composebasic/MainActivity.kt
+++ compose/jetpack-compose-basics/app/src/main/java/com/ldcc/composebasic/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ldcc.composebasic.ui.theme.ComposeBasicTheme
 
 // 3. Activity는 AndroidApp의 EntryPoint이다. setContent를 통해 layout을 정의한다.
@@ -31,9 +33,14 @@ class MainActivity : ComponentActivity() {
 }
 
 // 3. @Composable : Compose Function임을 알려주고 있는 어노테이션
+// 4. Surface를 통하여 Text의 배경색상을 변경할 수 있다.
+// 4. Text를 보면 자동으로 White 색상으로 변경되는 것을 확인할 수 있다. -> Material Component에서 지원해주는 기능
+// 4. UI 요소들은 modifier 파라미터를 가지고 있으며, Modifier는 UI의 배치방식들을 설명하고 있다.
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 // 3. Preview 어노테이션의 경우, 미리보기 화면을 제공한다.
