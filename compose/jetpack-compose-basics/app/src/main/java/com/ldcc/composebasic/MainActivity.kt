46fd2d1b4f5f336ab31901e1095b876c9db68627
hanseulchoi <lotte.hanseul@gmail.com>
2022-07-13 11:02:48 +0900

    6. Creating columns and rows, Columns와 Row를 이용하여 레이아웃 배치하기. weight속성 파악하기

--- compose/jetpack-compose-basics/app/src/main/java/com/ldcc/composebasic/MainActivity.kt
+++ compose/jetpack-compose-basics/app/src/main/java/com/ldcc/composebasic/MainActivity.kt
@@ -3,11 +3,8 @@ package com.ldcc.composebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,10 +31,13 @@ class MainActivity : ComponentActivity() {
 
 // 5. 계층이 깊어질수록 가독성이 떨어질 수 있다. 재사용이 가능한 UI를 최대한 작게 나누면서 읽기 쉽게하기 위해
 // MyApp 메소드를 생성하여 관리하여 가독성을 해결할 수 있다.
+// 6. Composable function도 Kotlin처럼 사용할 수 있다. (ex. for문 적용)
 @Composable
-private fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
@@ -45,15 +45,30 @@ private fun MyApp() {
 // 4. Surface를 통하여 Text의 배경색상을 변경할 수 있다.
 // 4. Text를 보면 자동으로 White 색상으로 변경되는 것을 확인할 수 있다. -> Material Component에서 지원해주는 기능
 // 4. UI 요소들은 modifier 파라미터를 가지고 있으며, Modifier는 UI의 배치방식들을 설명하고 있다.
+// 6. Column을 통해 레이아웃을 세로로 배치할 수 있다.
+// 6. Modifier에서 vertical과 horizontal로 부분적인 padding을 적용할 수 있다.
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text("Show more")
+            }
+        }
     }
 }
 
 // 3. Preview 어노테이션의 경우, 미리보기 화면을 제공한다.
-@Preview(showBackground = true)
+// 6. Preview 화면의 크기를 지정할 수 있다.
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeBasicTheme {
