fdf06f3afaf9e7a39f6cc050e2ea8a2abe23651b
hanseulchoi <lotte.hanseul@gmail.com>
2022-07-13 16:51:17 +0900

    13. Finishing touches!, IconButton과 String value 그리고 Card를 사용하였다.

--- compose/jetpack-compose-basics/app/src/main/java/com/ldcc/composebasic/MainActivity.kt
+++ compose/jetpack-compose-basics/app/src/main/java/com/ldcc/composebasic/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -11,10 +12,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -94,40 +99,92 @@ private fun Greetings(names: List<String> = List(1000){ "$it" } ) {
 // 7. 이를 해결하기위해, remember 객체(실제 내부에는 cache를 이용)를 통해 recomposition에도 데이터가 초기화되지 않게 한다.
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-
-    // 7. remember를 쓸 필요가 없다. 어차피 expanded가 기억되고 있기 때문
-    // 11. animateDpAsState를 이용하여 컴포즈 애니메이션 적용이 가능하다.
-    // 11. animationSpec을 통해서 애니메이션 커스텀이 가능하다.
-    val extraPadding by animateDpAsState(
-        if(expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
+//    val expanded = remember { mutableStateOf(false) }
+//
+//    // 7. remember를 쓸 필요가 없다. 어차피 expanded가 기억되고 있기 때문
+//    // 11. animateDpAsState를 이용하여 컴포즈 애니메이션 적용이 가능하다.
+//    // 11. animationSpec을 통해서 애니메이션 커스텀이 가능하다.
+//    val extraPadding by animateDpAsState(
+//        if(expanded.value) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+//        )
+//    )
+//
+//    Surface(
+//        color = MaterialTheme.colors.primary,
+//        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+//    ) {
+//        Row(modifier = Modifier.padding(24.dp)) {
+//            Column(
+//                modifier = Modifier
+//                    .weight(1f)
+//                    .padding(bottom = extraPadding.coerceAtLeast(0.dp)) // 11. spring animation 처리시 만약 음수까지 가게된다면 padding error 발생!
+//            ) {
+//                Text(text = "Hello, ")
+//                // 12. Text style 적용, 또한 copy를 이용해서 기존 style에 새로운 프로퍼티 추가
+//                Text(text = name, style = MaterialTheme.typography.h4.copy(
+//                    fontWeight = FontWeight.ExtraBold
+//                    )
+//                )
+//            }
+//
+//            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+//                Text(if(expanded.value) "Show less" else "Show more")
+//            }
+//        }
+//    }
+    // 13. Card Compose elements 이용
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp)) // 11. spring animation 처리시 만약 음수까지 가게된다면 padding error 발생!
-            ) {
-                Text(text = "Hello, ")
-                // 12. Text style 적용, 또한 copy를 이용해서 기존 style에 새로운 프로퍼티 추가
-                Text(text = name, style = MaterialTheme.typography.h4.copy(
-                    fontWeight = FontWeight.ExtraBold
-                    )
-                )
-            }
+        CardContent(name)
+    }
+}
 
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if(expanded.value) "Show less" else "Show more")
+// 13. String resource와 IconButton 사용
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if(expanded) {
+                Text(text = ("Composem ipsum color sit lazy, " +
+                        "padding theme elit, sed do bouncy. ").repeat(4))
             }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if(expanded) {
+                    stringResource(id = R.string.show_less)
+                } else {
+                    stringResource(id = R.string.show_more)
+                }
+            )
         }
     }
 }
@@ -145,7 +202,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text("Welcome to the Basics Codelab!")
-            Button(
+            IconButton(
                 modifier = Modifier.padding(vertical = 24.dp),
                 onClick = onContinueClicked
             ) {
